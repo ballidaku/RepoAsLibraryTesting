f3503489733a64eee6077a0ac415bd393cd72d52
Sharanpal Singh <sharan@upgame.co>
2023-04-03 18:27:47 +0530

    gradle changes

--- app/src/main/java/com/toaster/repoaslibrarytesting/MainActivity.kt
+++ app/src/main/java/com/toaster/repoaslibrarytesting/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.toaster.mylibrary.CommonMethods
 import com.example.repoaslibrarytesting.R
 import com.toaster.mylibrary.Toaster
 
